1b2308daf4e42c6df4b2e444d83f9756f2b3711f
leighlawnee <143223104+leighlawnee@users.noreply.github.com>
2024-04-12 22:19:51 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -5,14 +5,26 @@ public class Digits
 
 	private ArrayList<Integer> digitList;
 
-	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
+	public Digits(int num){
+	while (num>0){
+		digitList.add(0, num%10);
+		num = num/10;
+	}
+	if(num == 0)
+		digistList.add(num);
 	}
 
-	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		
+	public boolean isStrictlyIncreasing(){ /* to be implemented in part (b) */
+	for(int i = 0; i<digitList.size()-1; i++){
+		if(digitList.get(i)<digitList.get(i+1))
+			count++;
+		}
+
+		if(count == digitList.size()-1)
+			return true;
+		else
+			return false;
+
 
 	}
 	
